4751c4125b99706d9f508f9acf83438968c5eb9f
Acrownrity <ittatting@gmail.com>
2024-07-30 13:18:50 +0700

    update fast transection

--- services/insertDataFormDB.js
+++ services/insertDataFormDB.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const fs = require('fs-extra');
 
-async function insertDataFromFile(dbDir, filePath ,fileNameDB) {
+async function insertDataFromFile(dbDir, filePath, fileNameDB) {
     try {
         const dbPath = path.join(dbDir, fileNameDB);
         const db = new sqlite3.Database(dbPath);
@@ -11,25 +11,30 @@ async function insertDataFromFile(dbDir, filePath ,fileNameDB) {
         const data = await fs.readFile(filePath, 'utf-8');
         const words = data.split('\n').map(word => word.toLowerCase().trim());
 
-        // ใช้ Promise เพื่อทำให้การทำงานแบบ async เสร็จสมบูรณ์ก่อนปิดการเชื่อมต่อ
         await new Promise((resolve, reject) => {
-            // เตรียมคำสั่ง SQL สำหรับการเพิ่มข้อมูล
-            const stmt = db.prepare('INSERT INTO words (name) VALUES (?)');
+            db.serialize(() => {
+                db.run("BEGIN TRANSACTION");
 
-            // เพิ่มข้อมูลลงในฐานข้อมูล
-            words.forEach((word, index) => {
-                stmt.run(word, (err) => {
-                    if (err) reject(err);
-                });
-                console.log('add insert db:'+word)
-            });
+                // เตรียมคำสั่ง SQL สำหรับการเพิ่มข้อมูล
+                const stmt = db.prepare('INSERT INTO words (name) VALUES (?)');
 
-            // จบการเพิ่มข้อมูล
-            stmt.finalize((err) => {
-                if (err) reject(err);
-                db.close((err) => {
+                for (const word of words) {
+                    stmt.run(word, (err) => {
+                        if (err) reject(err);
+                    });
+                }
+
+                // จบการเพิ่มข้อมูล
+                stmt.finalize((err) => {
                     if (err) reject(err);
-                    resolve();
+
+                    db.run("COMMIT", (err) => {
+                        if (err) reject(err);
+                        db.close((err) => {
+                            if (err) reject(err);
+                            resolve();
+                        });
+                    });
                 });
             });
         });
